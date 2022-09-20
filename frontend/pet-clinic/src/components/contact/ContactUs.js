19ab15e9ba2dce81561ce24ed53bc065dbb187a0
WDomonkos <wdomi14@gmail.com>
2022-09-20 12:48:40 +0200

    css in contactus

--- frontend/pet-clinic/src/components/contact/ContactUs.js
+++ frontend/pet-clinic/src/components/contact/ContactUs.js
@@ -20,7 +20,7 @@ const ContactForm = () => {
     }
 
     return (
-        <form
+        <form className="contact-form"
             action={FORM_ENDPOINT}
             onSubmit={handleSubmit}
             method="POST"
@@ -53,12 +53,7 @@ const ContactForm = () => {
         />
             </div>
             <div className="mb-3 pt-0">
-                <button
-                    className="bg-blue-500 text-white active:bg-blue-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="submit"
-                >
-                    Send a message
-                </button>
+                <button type="button" className="btn btn-outline-secondary" data-mdb-ripple-color="dark">Send a message</button>
             </div>
         </form>
     );
