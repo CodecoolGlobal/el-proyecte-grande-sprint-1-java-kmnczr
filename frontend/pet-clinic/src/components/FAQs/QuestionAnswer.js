9453f43ded738b98d0cab5e8ed1ce24022ecea50
WDomonkos <wdomi14@gmail.com>
2022-10-04 11:08:10 +0200

    fixed qandA

--- frontend/pet-clinic/src/components/FAQs/QuestionAnswer.js
+++ frontend/pet-clinic/src/components/FAQs/QuestionAnswer.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import { Collapse } from 'antd';
-const { Panel } = Collapse;
+import React, {Component} from "react";
+import Faq from 'react-faq-component';
 
-function AppFaqs() {
-    return(
-        <div class="block faqBlock">
-            <div className="container-fluid">
-                <Collapse defaultActiveKey={['1']} className="faqs-panel">
-                    <Panel header="How can I add my animal to this website?" key="1">
-                        <p>You just need to be a user and then click Owners and add new pet. </p>
-                    </Panel>
-                    <Panel header="What kind of services does your clinic offer?" key="2">
-                        <p>hair cutting, nail cutting, washing, deworming</p>
-                    </Panel>
-                    <Panel header="How much does it cost to get my pet fixed" key="3">
-                        <p>We have a price list which you can look at on the Price list tab on the Home page</p>
-                    </Panel>
-                    <Panel header="How often do you suggest check-ups for my pet" key="4">
-                        <p>It depends on the animal. in the case of dogs, once a month or every two months, in the case of cats, it may be less often.</p>
-                    </Panel>
-                    <Panel header="Do I need an appointment" key="5">
-                        <p>If you have a profile we will contact you about an appointment.</p>
-                    </Panel>
-                </Collapse>
-            </div>
-        </div>
-    );
+const data = {
+    title: "FAQ (How it works)",
+    rows: [
+        {
+            title: "How can I add my animal to this website?",
+            content: "You just need to be a user and then click Owners and add new pet."
+        },
+        {
+            title: "What kind of services does your clinic offer?",
+            content: "hair cutting, nail cutting, washing, deworming"
+        },
+        {
+            title: "How much does it cost to get my pet fixed?",
+            content: "We have a price list which you can look at on the Price list tab on the Home page"
+        },
+        {
+            title: "How often do you suggest check-ups for my pet?",
+            content: "It depends on the animal. in the case of dogs, once a month or every two months, in the case of cats, it may be less often."
+        },
+        {
+            title: "Do I need an appointment?",
+            content: "If you have a profile we will contact you about an appointment."
+        },
+    ]
 }
 
-export default AppFaqs
+export  default class QuestionAnswer extends Component {
+    render() {
+        return (
+            <div>
+                <Faq data={data}/>
+            </div>
+        )
+    }
+}
